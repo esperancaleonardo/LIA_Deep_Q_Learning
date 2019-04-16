4e9999f44430940066badd16c2d714e73bb002af
Leonardo Esperanca <leonardo.oliveira.esperanca@gmail.com>
2019-04-16 17:07:35 -0400

    limpando arquivos

--- root/utils.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-
-
-def bbPlotter(image, bbContours):
-
-    for bb in bbContours:
-        cv.rectangle(image,(bb[0]-15,bb[1]-15), (bb[0]+15,bb[1]+15), (0x00,0x00,0xff), 1)
-
-    return image

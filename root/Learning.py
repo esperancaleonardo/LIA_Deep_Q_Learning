f964a57e6b5106a76ffd436bf8e71e02c00c8383
Leonardo Esperança <leonardo.oliveira.esperanca@gmail.com>
2019-08-29 12:29:12 -0400

    corrigindo ident

--- root/Learning.py
+++ root/Learning.py
@@ -106,7 +106,7 @@ class Learning(object):
 
             end = time.time()
             self.agent.controller.stop_sim()
-            #sleep(1)
+            sleep(2)
 
             evall = None
             if len(self.agent.memory) > int(self.agent.batch_size):

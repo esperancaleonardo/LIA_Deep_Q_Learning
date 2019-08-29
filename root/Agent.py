f832a16eb69936a0f7ff75c1288786ca25ddaaf5
Leonardo Esperança <leonardo.oliveira.esperanca@gmail.com>
2019-08-29 12:00:01 -0400

    corrigindo ident

--- root/Agent.py
+++ root/Agent.py
@@ -207,7 +207,7 @@ class Agent(object):
 
             if distance >= 11.0:
                 done = 0
-                reward = -1*abs(distance) if distance >= 50.0 else base - distance
+                reward = -2*abs(distance) if distance >= 50.0 else base - distance
             else:
                 self.done_counter +=1
                 done = 1
